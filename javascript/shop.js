ea369df8787dbcbbeba354499c718a9d21a1da40
User@Desktop-3D4BBRS <kebrew2@gmail.com>
2018-03-27 15:23:12 -0400

    added comments to code

--- javascript/shop.js
+++ javascript/shop.js
@@ -1,3 +1,6 @@
+
+//function once button(picture) is clicked modal will open
+//gets the right modal using data-modal class
 var modalBtns = [...document.querySelectorAll(".button")];
 modalBtns.forEach(function(btn){
   btn.onclick = function() {
@@ -6,6 +9,7 @@ modalBtns.forEach(function(btn){
   }
 });
 
+//function for the modal to close once (x) is clicked in the modal
 var closeBtns = [...document.querySelectorAll(".close")];
 closeBtns.forEach(function(btn){
   btn.onclick = function() {
@@ -14,6 +18,7 @@ closeBtns.forEach(function(btn){
   }
 });
 
+//function for the modal to close if anywhere outside the modal is clicked
 window.onclick = function(event) {
   if (event.target.className === "modal") {
     event.target.style.display = "none";
